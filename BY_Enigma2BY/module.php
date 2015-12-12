d2903b312ba8e966a6b39645993f461d51ee0c3e
BayaroX <github_com@heaven-server.de>
2015-12-12 17:22:41 +0100

    Change

--- BY_Enigma2BY/module.php
+++ BY_Enigma2BY/module.php
@@ -583,24 +583,24 @@ class Enigma2BY extends IPSModule
         IPS_SetVariableProfileIcon($Name, $Icon);
         IPS_SetVariableProfileText($Name, $Prefix, $Suffix);
         IPS_SetVariableProfileValues($Name, $MinValue, $MaxValue, $StepSize);
-        
     }
     
-    protected function RegisterProfileInteger($Name, $Icon, $Prefix, $Suffix, $MinValue, $MaxValue, $StepSize) {
-        
-        if(!IPS_VariableProfileExists($Name)) {
-            IPS_CreateVariableProfile($Name, 1);
-        } else {
-            $profile = IPS_GetVariableProfile($Name);
-            if($profile['ProfileType'] != 1)
-            throw new Exception("Variable profile type does not match for profile ".$Name);
-        }
-        
-        IPS_SetVariableProfileIcon($Name, $Icon);
-        IPS_SetVariableProfileText($Name, $Prefix, $Suffix);
-        IPS_SetVariableProfileValues($Name, $MinValue, $MaxValue, $StepSize);
-        
-    }
+    protected function RegisterProfileInteger($Name, $Icon, $Prefix, $Suffix, $MinValue, $MaxValue, $StepSize)
+		{
+				if (!IPS_VariableProfileExists($Name))
+      	{
+      			IPS_CreateVariableProfile($Name, 1);
+      	}
+      	else
+      	{
+      			$profile = IPS_GetVariableProfile($Name);
+            if ($profile['ProfileType'] != 1)
+            		throw new Exception("Variable profile type does not match for profile " . $Name);
+      	}
+      	IPS_SetVariableProfileIcon($Name, $Icon);
+      	IPS_SetVariableProfileText($Name, $Prefix, $Suffix);
+      	IPS_SetVariableProfileValues($Name, $MinValue, $MaxValue, $StepSize);
+		}
     
     protected function RegisterProfileIntegerEx($Name, $Icon, $Prefix, $Suffix, $Associations) {
         if ( sizeof($Associations) === 0 ){
