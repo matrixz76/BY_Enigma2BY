7fc9cb8b64277debab0e0e4cbb0407b2fdaa566a
BayaroX <github_com@heaven-server.de>
2015-12-12 20:36:16 +0100

    Fix "GetPowerState"

--- BY_Enigma2BY/module.php
+++ BY_Enigma2BY/module.php
@@ -325,7 +325,7 @@ class Enigma2BY extends IPSModule
     		{
 		    		$url = "http://".$IP."/web/powerstate";
 						$xml = @simplexml_load_file($url);
-						if ($xml->e2instandby == false)
+						if ($xml->e2instandby == "false")
 						{
 								$PowerStateIST = 1;
 								$this->SetValueInteger("PowerStateVAR", 1); // AN
